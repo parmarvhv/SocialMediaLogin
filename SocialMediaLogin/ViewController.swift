d7f0846313cd007a6a30c1d1a53e14f1ab925950
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-10-26 13:56:46 +0530

    setup project files

---
+++ SocialMediaLogin/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SocialMediaLogin
+//
+//  Created by Vaibhav Parmar on 26/10/17.
+//  Copyright © 2017 Vaibhav Parmar. All rights reserved.
+//
+
+import UIKit
+
+class SocialMediaLoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func handleGoogleConnectTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleFBConnectTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleTwitterTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func handleLinkedINTapped(_ sender: Any) {
+        
+    }
+}
+
