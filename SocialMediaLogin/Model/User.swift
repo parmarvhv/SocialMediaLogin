86d4686ff4e6284d56b76754213d7fa99091f7e5
Vaibhav Parmar <vaibhav@nickelfox.com>
2017-10-26 17:06:35 +0530

    implment facebook login and add User class

--- SocialMediaLogin/Model/User.swift
+++ SocialMediaLogin/Model/User.swift
@@ -8,20 +8,24 @@
 
 import Foundation
 
-//public class User {
-//    var firstName: String
-//    var lastName: String
-//    var email: String?
-//    
-//    public init(firstName: String, lastName: String, email: String?) {
-//        self.firstName = firstName
-//        self.lastName = lastName
-//        self.email = email
-//    }
-//}
-
-public protocol User {
+public protocol UserDelegate {
     var firstName:String { get }
     var lastName:String { get }
-    var email:String { get }
+    var email:String? { get }
+}
+
+public class User {
+    public var firstName: String
+    public var lastName: String
+    public var email: String?
+    
+    public init(firstName: String, lastName: String, email: String?) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+    }
+}
+
+extension User: UserDelegate {
+    
 }
